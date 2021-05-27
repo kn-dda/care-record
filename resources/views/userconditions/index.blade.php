f07c5bac7c757cea9c73eeb16162f732caf357a0
Ubuntu <ubuntu@ip-172-31-24-154.ap-northeast-1.compute.internal>
2021-05-27 06:32:15 +0000

    modified index.blade.php

--- resources/views/userconditions/index.blade.php
+++ resources/views/userconditions/index.blade.php
@@ -9,31 +9,19 @@
         <thead>
             <tr>
                 <th>id</th>
-            </tr>
-            <tr>
                 <th>起床時刻</th>
-            </tr>
-            <tr>
                 <th>体温</th>
-            </tr>
-            <tr>
                 <th>薬</th>
-            </tr>
-            <tr>
                 <th>食事量</th>
-            </tr>
-            <tr>
                 <th>酸素濃度</th>
-            </tr>
-            <tr>
                 <th>血圧</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($user_conditions as $user_condition)
             <tr>
-                <td>{!! link_to_route('user_condition.store', $user_condition->id, ['user_condition' => $user_condition->id]) !!}</td> //ルーティング
-                <td>{{ $user_condition->id }}</td>
+                {{-- user_condition.store ルーティング --}}
+                <td>{!! link_to_route('user_condition.store', $user_condition->id, ['user_condition' => $user_condition->id]) !!}</td>
                 <td>{{ $user_condition->wake }}</td>
                 <td>{{ $user_condition->temperature }}</td>
                 <td>{{ $user_condition->medicine }}</td>
@@ -47,18 +35,3 @@
     @endif
 </div>
 @endif
-
-{{-- idについてエラーが出るため、試験的にコメントオフ --}}
-{{-- <h1>id = {{ $user_conditions->id }} の記録詳細ページ</h1> --}}
-
-{{-- @extends('layouts.app')
-
-@section('content') --}}
-
-{{-- <h1>id = {{ $user_conditions->id }} の記録詳細ページ</h1> --}}
-
-    {{-- メッセージ編集ページへのリンク --}}
-    {{-- {!! link_to_route('user_conditions.edit', 'このメッセージを編集', ['user_condition' => $user_conditions->id], ['class' => 'btn btn-light']) !!} --}}
-
-{{-- 
-@endsection --}}
